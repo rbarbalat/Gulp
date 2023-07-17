8508d3a17f05788d690ee70c539a06a67962cdf1
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-17 19:07:31 -0400

    attempted to half style a star

--- react-app/src/components/StarRatingInput/index.js
+++ react-app/src/components/StarRatingInput/index.js
@@ -20,7 +20,15 @@ export default function StarRatingInput({ rating, onRatingChange, form })
 
   return (
       <div className="rating-input">
-        <div className={activeRating >= 1 ? "filled" : "empty"}>
+        {/* <div className={activeRating >= 1 ? "filled" : "empty"}> */}
+        <div className =
+            {
+                form ?
+                (activeRating >= 1 ? "filled" : "empty")
+                :
+                (activeRating >= 0.5 ? (activeRating >= 3 ? "filled" : "partial") : "empty" )
+            }
+        >
           <i className="fa-regular fa-star"
             onMouseEnter={() => onMouseEnter(1)}
             onMouseLeave={onMouseLeave}
@@ -28,28 +36,60 @@ export default function StarRatingInput({ rating, onRatingChange, form })
             >
             </i>
         </div>
-        <div className={activeRating >= 2 ? "filled" : "empty"}>
+        {/* <div className={activeRating >= 2 ? "filled" : "empty"}> */}
+        <div className =
+            {
+                form ?
+                (activeRating >= 2 ? "filled" : "empty")
+                :
+                (activeRating >= 1.5 ? (activeRating >= 3 ? "filled" : "partial") : "empty" )
+            }
+        >
             <i className="fa-regular fa-star"
               onMouseEnter={() => onMouseEnter(2)}
               onMouseLeave={onMouseLeave}
               onClick ={() => onRatingChange(2)}>
             </i>
         </div>
-        <div className={activeRating >= 3 ? "filled" : "empty"}>
+        {/* <div className={activeRating >= 3 ? "filled" : "empty"}> */}
+        <div className=
+            {
+                form ?
+                (activeRating >= 3 ? "filled" : "empty")
+                :
+                (activeRating >= 2.5 ? (activeRating >= 3 ? "filled" : "partial") : "empty" )
+            }
+        >
             <i className="fa-regular fa-star"
             onMouseEnter={() => onMouseEnter(3)}
             onMouseLeave={onMouseLeave}
             onClick ={() => onRatingChange(3)}>
             </i>
         </div>
-        <div className={activeRating >= 4 ? "filled" : "empty"}>
+        {/* <div className={activeRating >= 4 ? "filled" : "empty"}> */}
+        <div className =
+            {
+                form ?
+                (activeRating >= 4 ? "filled" : "empty")
+                :
+                (activeRating >= 3.5 ? (activeRating >= 3 ? "filled" : "partial") : "empty" )
+            }
+        >
             <i className="fa-regular fa-star"
               onMouseEnter={() => onMouseEnter(4)}
               onMouseLeave={onMouseLeave}
               onClick ={() => onRatingChange(4)}>
               </i>
         </div>
-        <div className={activeRating >= 5 ? "filled" : "empty"}>
+        {/* <div className={activeRating >= 5 ? "filled" : "empty"}> */}
+        <div className =
+            {
+                    form ?
+                    (activeRating >= 5 ? "filled" : "empty")
+                    :
+                    (activeRating >= 4.5 ? (activeRating >= 3 ? "filled" : "partial") : "empty" )
+            }
+        >
               <i className="fa-regular fa-star"
                 onMouseEnter={() => onMouseEnter(5)}
                 onMouseLeave={onMouseLeave}
