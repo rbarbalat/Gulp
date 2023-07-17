44161366bcb1d3d93d625502d15b4cdb9dd225aa
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-17 14:28:40 -0400

    added background Image in line css styles to the TopCard

--- react-app/src/components/TopCard/index.js
+++ react-app/src/components/TopCard/index.js
@@ -4,8 +4,16 @@ export default function TopCard({business})
 {
     if(Object.keys(business).length === 0) return <div>loading</div>
     // return <div>Hello World!!! from business {business.id}</div>
+
+    //the non variable styles can be moved to the css file later
+    const styles = {
+        backgroundImage: `url(${business.preview_image})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+    }
     return(
-        <div className = "top_card_wrapper">
+        <div className = "top_card_wrapper" style={styles}>
             <div className = "top_info_wrapper">
                 <div className = "single_bus_name">{business.name}</div>
                 <div className = "single_bus_rating">{business.average} stars and <span>{business.numReviews} reviews</span></div>
