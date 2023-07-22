54c95759f19e80159e4599f3cf2035058d796ed9
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-21 20:02:37 -0400

    adjusted business and bus image seeders

--- app/seeds/bus_images.py
+++ app/seeds/bus_images.py
@@ -5,19 +5,42 @@ fake = Faker()
 from random import choice, sample, randint
 
 def seed_bus_images(businesses):
-    urls = [
-        "https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/3/3a/Chef%27s_table_at_Marcus.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/c/cd/Desire_Oyster_Bar_interior.jpg"
+    urls_0 = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/earl_opt_1.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/earl_opt_2.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/earl_opt_3.jpeg"
     ]
-    for i in range(len(businesses)):
+    urls_1 = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/gundis_opt_1.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/gundis_opt_2.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/gundis_opt_3.jpeg"
+    ]
+    urls_2 = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/mastros_opt_1.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/mastros_opt_2.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/mastros_opt_3.jpeg"
+    ]
+    urls_3 = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sappori_opt_1.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sappori_opt_2.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sappori_opt_3.jpeg"
+    ]
+    urls_4 = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sunda_opt_1.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sunda_opt_2.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sunda_opt_3.jpeg"
+    ]
+    urls_5 = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/frontera_opt_1.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/frontera_opt_2.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/frontera_opt_3.jpeg"
+    ]
+    urls = [urls_0, urls_1, urls_2, urls_3, urls_4, urls_5]
+
+    for i in range(6):
         for j in range(3):
-            bus_image = BusImage(
-                business = businesses[i],
-                # url = fake.url()
-                url = urls[j]
-            )
-            db.session.add(bus_image)
+            bus_image = BusImage(business = businesses[i], url = urls[i][j])
+
     db.session.commit()
     print("seeded business images")
 
--- app/seeds/business.py
+++ app/seeds/business.py
@@ -7,9 +7,9 @@ from random import choice, sample, randint
 def seed_businesses(users):
     urls = [
         "https://bucket-rb22.s3.us-east-2.amazonaws.com/earl.jpeg",
-        "https://bucket-rb22.s3.us-east-2.amazonaws.com/brueggers.png",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/gundis.jpeg",
         "https://bucket-rb22.s3.us-east-2.amazonaws.com/mastros.jpeg",
-        "https://bucket-rb22.s3.us-east-2.amazonaws.com/mesa.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/sappori.jpeg",
         "https://bucket-rb22.s3.us-east-2.amazonaws.com/sunda_1.jpeg",
         "https://bucket-rb22.s3.us-east-2.amazonaws.com/frontera_2.jpeg"
     ]
@@ -51,23 +51,23 @@ def seed_businesses(users):
     ]
     names = [
         "The Earl of Sandwich",
-        "Bruegger's Bagels",
+        "The Gundis",
         "Mastro's Steakhouse",
-        "Mesa grill",
+        "Sapori Trattoria",
         "Sunda",
         "Frontera"
     ]
     descriptions = [0, 1, 2, 3, 4, 5]
     descriptions[0] = "Some 250 years after the invention of the sandwich, Earl of Sandwich® restaurants have embraced the idea and crafted it into a made-to-order, freshly baked sandwich like no other. Our menu pays tribute to the art of the sandwich. From our signature namesakes like The Original 1762® and The Full Montagu, to our fresh salads, we believe in using the highest quality ingredients in everything we serve. We believe that sandwiches are more than a convenience food; they should be carefully crafted and thoroughly enjoyed."
-    descriptions[1] = "Now in our 35th year, Bruegger’s Bagels operates nearly 260 bakeries nationwide. Our menu has grown to include sandwiches for breakfast and lunch, garden-fresh salads, hearty soups, desserts and custom-roasted coffee — but the essence of Bruegger’s hasn’t changed. We’re still committed to serving genuine, New York-style bagels and real, made-in-Vermont cream cheese. Because that’s not just what we do; it’s where we come from."
+    descriptions[1] = "A collaboration between Mehmet Duzgun, Veysel Yildirim, Mehmet Yavuz, Denisse and her father, Executive Chef Juan González, The Gundis Kurdish Kitchen is where Kurdish cuisine and the talent of our Chef make their Chicago debut. Here, you can sip black tea and wine, and enjoy the robust flavors of a kitchen that celebrates and shares the diversity of the food and culture of Kurds."
     descriptions[2] = 'Consistently hailed by diners and critics alike as "masterful" (Los Angeles Daily News), "high end" ( Los Angeles Times), one of the "top 10 steakhouse in the U.S." (Gayot) and "the preferred steakhouse of celebs and locals" (944), Mastro`s Restaurants, LLC is a collection of sophisticated, classic steakhouses and sumptuous fish houses.'
-    descriptions[3] = "The combination of Mesa Grill's magnificent decor, the exciting vibe of the casino atmosphere and Bobby Flay's stunning range of textures, flavors and colors incorporated into his menu continue to electrify diners year after year."
+    descriptions[3] = "We're a cozy Italian Restaurant serving fresh pasta & other authentic Italian dishes in Chicago's Lincoln Park & Lakeview Neighborhoods. Sapori Trattoria is where love for food, warmth & delight combine. Our small neighborhood eatery is relaxed and intimate, which creates the perfect backdrop for a first date, an anniversary dinner, and a warm family gathering."
     descriptions[4] = "At Sunda, we pride ourselves on taking classic, culturally-important dishes from all over Southeast Asia and modernizing each one by upgrading ingredients, cooking techniques and presentation to create a tasting experience that is completely new while still retaining the deep-rooted spirit of the original."
     descriptions[5] = "It was 1987. Rick Bayless and his wife, Deann Groen Bayless, had just returned from an extended stay in Mexico, where they had been researching their first book. They wanted a restaurant that tasted and felt like their travels. So they hung colorful Mexican folk art on the walls, turned up the Mariachi music and packed the menu with the foods that reminded them of their travels:  tangy tomatillos, rich black beans, fiery chiles. And that’s how it’s been ever since. Not that we haven’t changed. We change all the time, adding new authentic flavors to our menus, developing new relationships with the Midwestern farmers who provide us with everything from summer squash to maple syrup to goat. But at the end of the day, no matter how inventive our techniques or local our ingredients, our food still transports you to the same place: the colorful, vibrant kitchens of Mexico."
     lst = []
     for i in range(6):
         bus = Business(
-            owner = choice(users),
+            owner = users[i],
             name = names[i],
             description = descriptions[i],
             prev_url = urls[i],
