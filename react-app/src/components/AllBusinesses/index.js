6b480e7bb4d4557566c372fe68a2c14e0a768c32
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-22 20:04:02 -0400

    some work on all bus

--- react-app/src/components/AllBusinesses/index.js
+++ react-app/src/components/AllBusinesses/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // import { useHistory } from "react-router-dom";
 import { thunkLoadBusinesses } from "../../store/business";
@@ -13,7 +13,33 @@ export default function AllBusinesses()
     //initialized to an empty array before the useEffect runs, singe intialState = {}
     const businesses = useSelector(state => Object.values(state.businesses.allBus))
     const user = useSelector(state => state.session.user);
-    // const history = useHistory();
+
+    const [sort, setSort] = useState("low");
+
+    //businesses an empty array before the thunk is dispatched so can call sort on it
+    if(sort === "new") businesses.sort((a,b) => {
+        return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+    });
+
+    if(sort === "old") businesses.sort((a,b) => {
+        return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+    });
+
+    if(sort === "high") businesses.sort((a,b) => {
+        //average is null if numReviews is 0, sort those to the back
+        if(!a && b) return 1;
+        if(a && !b) return -1;
+        return b.average - a.average;
+    });
+
+    if(sort === "low") businesses.sort((a,b) => {
+        if(!a && b) return 1;
+        if(a && !b) return -1;
+        return a.average - b.average;
+    });
+    console.log("the sorted array is --- ");
+    console.log(businesses);
+
     const dispatch = useDispatch();
     useEffect(() => {
         //if(Number(business_id) !== business.id)
@@ -26,8 +52,7 @@ export default function AllBusinesses()
         fetchData()
     }, [dispatch])
 
-
-
+    //if you delete all businesses, would show loading, need to change
     if(businesses.length === 0) return <div>loading</div>
     return (
         <div className = "all_bus_wrapper">
