728d8c8ae8b4788e1a45d83a322eca752c07e05b
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-23 18:44:14 -0400

    migrated the new tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,6 @@ from .business import Business
 from .review import Review
 from .bus_images import BusImage
 from .rev_images import RevImage
+from .favorite import Favorite
+from .reply import Reply
 from .db import environment, SCHEMA
