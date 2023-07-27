7685b419388edc4cf5878783f4bd0aec7326cdd4
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-27 14:29:48 -0400

    started favorites routes

--- app/api/fav_routes.py
+++ app/api/fav_routes.py
@@ -5,5 +5,22 @@ from app.models import db, Favorite, Business, User
 fav_routes = Blueprint("favorites", __name__)
 
 @fav_routes.route("/")
-def get_all_favorites():
+def get_all_favorites_by_user():
     pass
+
+#DELETE favorite by Id
+@fav_routes.route("/<int:id>", methods = ["DELETE"])
+def delete_fav_by_id(id):
+    if not current_user.is_authenticated:
+        return {"error": "not authenticated"}, 401
+
+    fav = Favorite.query.get(id)
+    if not fav:
+        return {"error": "Favorite not found"}, 404
+
+    if current_user.id != fav.user_id:
+        return {"error": "Not authorized"}
+
+    db.session.delete(fav)
+    db.session.commit()
+    return {"message": "Successfully deleted the favorite"}
