fb1e3d2fdf62fae82b9705ad844dcd36d893585f
Roman Barbalat <roman.barbalat22@gmail.com>
2023-08-07 19:14:02 -0400

    created file input component

--- react-app/src/components/FileInput/index.js
+++ react-app/src/components/FileInput/index.js
@@ -1,10 +1,23 @@
 
 
-export default function FileInput({edit, image, image_url})
+export default function FileInput( {url, image, upload, deleteImage, num} )
 {
-    return <div>hello world</div>
+    return (
+        <p>
+        {
+            !url &&
+            <p className = "bus_form_upload_icon_wrapper">
+                <i className={image ? "fa-solid fa-pen-to-square" : "fa-solid fa-upload"} onClick = {upload}></i>
+            </p>
+        }
+        {
+            url &&
+            <p className="image_and_delete_button">
+                <i className={image ? "fa-solid fa-pen-to-square" : "fa-solid fa-upload"} onClick = {upload}></i>
+                <i className={image ? "fa-solid fa-trash hidden" : "fa-solid fa-trash"} onClick={() => deleteImage(num)}></i>
+                <img alt={"optional " + num} className={ image ? "form_images hidden" : "form_images"} src={url}></img>
+            </p>
+        }
+        </p>
+    )
 }
-
-// const FileInput = React.forwardRef((props,ref) => {
-// })
-// export default FileInput;
