eac40c416d44b264491887392c20c347a2209acc
Roman Barbalat <roman.barbalat22@gmail.com>
2023-08-08 17:58:10 -0400

    seeded users

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -28,24 +28,71 @@ def seed_users():
     # db.session.commit()
 
     lst = [demo, marnie, bobbie]
-    num = 16
+    # num = 16
 
-    names = []
-    emails = []
-    count = 0
-    while count <= num:
-        name = fake.name().split(" ")[0]
-        email = f"{name}@aa.io"
-        if name not in names:
-            names.append(name)
-            emails.append(email)
-            count = count + 1
-
-    for i in range(num):
+    names = [
+        "Anna",
+        "Arthur",
+        "Brian",
+        "Barbara",
+        "Carl",
+        "Clarissa",
+        "Dennis",
+        "Danielle",
+        "Eric",
+        "Erica",
+        "Fred",
+        "Faith",
+        "Gary",
+        "Gina",
+        "Hector",
+        "Heather"
+    ]
+    emails = [
+        "anna@aa.io",
+        "arthur@aa.io",
+        "brian@aa.io",
+        "barbara@aa.io",
+        "carl@aa.io",
+        "clarissa@aa.io",
+        "dennis@aa.io",
+        "danielle@aa.io",
+        "eric@aa.io",
+        "erica@aa.io",
+        "fred@aa.io",
+        "faith@aa.io",
+        "gary@aa.io",
+        "gina@aa.io",
+        "hector@aa.io",
+        "heather@aa.io"
+    ]
+    urls = [
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/demo.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/marnie.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/bobbie.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/anna.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/arthur.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/brian.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/barbara.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/carl.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/clarissa.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/dennis.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/danielle.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/eric.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/erica.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/fred.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/faith.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/gary.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/gina.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/hector.jpeg",
+        "https://bucket-rb22.s3.us-east-2.amazonaws.com/heather.jpeg",
+    ]
+    for i in range(len(names)):
         user = User(
             username = names[i],
             email = emails[i],
             password = "password",
+            url = urls[i],
             created_at = datetime(2022, 11, i + 1, 15)
         )
         db.session.add(user)
