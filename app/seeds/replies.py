b33eb3c4bf21a83c86169bb42dfa747d316cf779
Roman Barbalat <roman.barbalat22@gmail.com>
2023-08-08 21:53:56 -0400

    done adjusting seeds

--- app/seeds/replies.py
+++ app/seeds/replies.py
@@ -23,8 +23,8 @@ def seed_replies(reviews):
         "Thank you for your wonderful review.  I'm so happy that you enjoyed all your meals at the Earl of Sandwich.  Its customers like you that make this job worth doing!",
         "The lamb shank is my favorite item on the menu as well!  Thank you for dining at The Gundis!",
         "No better combo than steak & seafood.  Give the King Crab legs a try next time.  Thanks for your nice review!",
-        "I'm sorry your meal was only average.  We strive to make exceptional dishes and we hope that you come back for a second try!",
-        "Sorry to hear that your order was messed up.  Happy to hear that you enjoyed the food.  I will send you a message about a free meal as compensation",
+        "I'm sorry your meal was only average.  We strive to make exceptional dishes and we hope that you give us another shot!",
+        "Sorry to hear that your order was messed up.  Happy to hear that you enjoyed the food.  I will send you a message about a free meal as compensation.",
         "We definitely strive for both taste and authenticity.  Thanks for the five star review!"
     ]
     selected_reviews = [
