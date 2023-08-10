02ba655f67f6e59027196425881c8995e676edde
Roman Barbalat <roman.barbalat22@gmail.com>
2023-08-10 14:54:17 -0400

    changes in the helpers file

--- react-app/src/helpers/index.js
+++ react-app/src/helpers/index.js
@@ -9,36 +9,26 @@ import { authenticate } from "../store/session";
 
 export async function linkEditReview(review_id, dispatch, history)
 {
-    const res = await dispatch(thunkLoadSingleReview(review_id));
-
     // singleRev will be in the store so the useSelectors on the
     // edit form will be able to get the existing values right away
-    if(res.error)
-    {
-    }else{
-        history.push(`/reviews/${review_id}`);
-        return;
-    }
+
+    const res = await dispatch(thunkLoadSingleReview(review_id));
+    if(!res.error) return history.push(`/reviews/${review_id}`);
 }
 
 export async function deleteReview(review_id, dispatch, business_id)
 {
     const res = await dispatch(thunkDeleteReview(review_id));
-    if(res.error)
+    if(!res.error)
     {
-    }else {
         if(business_id)
         {
             //if you are deleting from the single business page
             //the single business component pulls reviews from the business
             //store not the review store so need to trigger an update
-            const res_two = await dispatch(thunkLoadSingleBusiness(business_id));
-            if(res_two.error)
-            {
-            }else {
-            }
+            await dispatch(thunkLoadSingleBusiness(business_id));
         }
-        //after deleting a business or review, need to pull new usr session info
+        //after deleting a business or review, need to pull new user session info
         //to get his updated numReviews/numBusinesses for the user profile
         await dispatch(authenticate());
         return;
@@ -49,48 +39,31 @@ export async function linkEditBus(business_id, dispatch, history)
 {
     //load the singleBus in the store so the useSelector in
     //the edit form can access it right away
+
     const res = await dispatch(thunkLoadSingleBusiness(business_id));
-    if(res.error)
-    {
-    }else{
-        history.push(`/businesses/${business_id}/edit`);
-    }
+    if(!res.error) return history.push(`/businesses/${business_id}/edit`);
 }
 
 export async function deleteBusiness(business_id, user_id, dispatch, history)
 {
     const res = await dispatch(thunkDeleteBusiness(business_id));
-    if(res.error)
+    if(!res.error)
     {
-    }else {
         //owner linked back to his profile after deleting a business
-
         //after deleting a business or review (from the user profile), need to pull new user session info
         //to get the users updated numReviews/numBusinesses for the user profile
         await dispatch(authenticate());
-        history.push(`/users/${user_id}`);
-        return;
+        return history.push(`/users/${user_id}`);
     }
 }
 
 export async function deleteReply(reply_id, dispatch, business_id)
 {
     const res = await dispatch(thunkDeleteReply(reply_id));
-    if(res.error)
-    {
-    }else {
-        if(business_id)
-        {
-            //if you are deleting from the single business page
-            //the single business component pulls replies from the business
-            //store not the reply store so need to trigger an update
-            const res_two = await dispatch(thunkLoadSingleBusiness(business_id));
-            if(res_two.error)
-            {
-            }else {
-            }
-        }
-    }
+    //for now replies only appear on the single bus component
+    //the single business component pulls replies from the business store
+    //not the reply store so need to trigger an update
+    if(!res.error && business_id) await dispatch(thunkLoadSingleBusiness(business_id));
 }
 
 export async function createFavorite(business_id, user_id, pathname, dispatch)
@@ -100,13 +73,11 @@ export async function createFavorite(business_id, user_id, pathname, dispatch)
         const res = await fetch(`/api/businesses/${business_id}/favorites`, options);
         if(res.ok)
         {
-            const data = await res.json();
+            //user favorites data are pulled from the session store
             await dispatch(authenticate());
             if(pathname === "/businesses") await dispatch(thunkLoadBusinesses());
             if(pathname === `businesses/${business_id}`) await dispatch(thunkLoadSingleBusiness(business_id));
             if(pathname === `/users/${user_id}`) await dispatch(thunkLoadFavBusinessesOfUser())
-        }else{
-            const error_data = await res.json();
         }
     }catch(error){
     }
@@ -118,11 +89,11 @@ export async function deleteFavorite(favorite_id, user_id, pathname, dispatch, b
         const res = await fetch(`/api/favorites/${favorite_id}`, options);
         if(res.ok)
         {
+            //user favorites data are pulled from the session store
             await dispatch(authenticate());
             if(pathname === "/businesses") await dispatch(thunkLoadBusinesses());
             if(pathname === `businesses/${business_id}`) await dispatch(thunkLoadSingleBusiness(business_id));
             if(pathname === `/users/${user_id}`) await dispatch(thunkLoadFavBusinessesOfUser());
-        }else{
         }
     }catch(error){
     }
